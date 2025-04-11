eb60e692d866e722c7c2333618d06d6ede229b13
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-04-11 14:37:21 -0300

    realizado o desenvolvimento do método Cadastrar

--- ProjetoEcommerce/Repositorio/ProdutoRepositorio.cs
+++ ProjetoEcommerce/Repositorio/ProdutoRepositorio.cs
@@ -8,7 +8,7 @@ namespace ProjetoEcommerce.Repositorio
     {
         private readonly string _conexaoMySQL = configuration.GetConnectionString("ConexaoMySQL");
 
-        public void Cadastrar(Cliente cliente)
+        public void Cadastrar(Produto produto)
         {
             // Bloco using para garantir que a conexão seja fechada e os recursos liberados após o uso
             using (var conexao = new MySqlConnection(_conexaoMySQL))
@@ -16,13 +16,15 @@ namespace ProjetoEcommerce.Repositorio
                 // Abre a conexão com o banco de dados MySQL
                 conexao.Open();
                 // Cria um novo comando SQL para inserir dados na tabela 'cliente'
-                MySqlCommand cmd = new MySqlCommand("insert into cliente (NomeCLi,TelCli,EmailCli) values (@nome, @telefone, @email)", conexao); // @: PARAMETRO
+                MySqlCommand cmd = new MySqlCommand("insert into cliente (NomeProd,Descricao,Quantidade,Preco) values (@nomeProd, @descricao, @quantidade, @preco)", conexao); // @: PARAMETRO
                                                                                                                                                  // Adiciona um parâmetro para o nome, definindo seu tipo e valor
-                cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = cliente.NomeCli;
+                cmd.Parameters.Add("@nomeProd", MySqlDbType.VarChar).Value = produto.NomeProd;
                 // Adiciona um parâmetro para o telefone, definindo seu tipo e valor
-                cmd.Parameters.Add("@telefone", MySqlDbType.VarChar).Value = cliente.TelCli;
+                cmd.Parameters.Add("@descricao", MySqlDbType.VarChar).Value = produto.Descricao;
                 // Adiciona um parâmetro para o email, definindo seu tipo e valor
-                cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = cliente.EmailCli;
+                cmd.Parameters.Add("@quantidade", MySqlDbType.Int32).Value = produto.Quantidade;
+
+                cmd.Parameters.Add("@preco", MySqlDbType.Decimal).Value = produto.Preco;
                 // Executa o comando SQL de inserção e retorna o número de linhas afetadas
                 cmd.ExecuteNonQuery();
                 // Fecha explicitamente a conexão com o banco de dados (embora o 'using' já faça isso)
