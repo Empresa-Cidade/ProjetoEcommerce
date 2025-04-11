5bf1ba64da88deb14c765111fd2032d113ddbb9f
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-04-11 14:38:35 -0300

    criado o método Atualizar

--- ProjetoEcommerce/Repositorio/ProdutoRepositorio.cs
+++ ProjetoEcommerce/Repositorio/ProdutoRepositorio.cs
@@ -31,6 +31,40 @@ namespace ProjetoEcommerce.Repositorio
                 conexao.Close();
             }
         }
+        public bool Atualizar(Cliente cliente)
+        {
+            try
+            {
+                // Bloco using para garantir que a conexão seja fechada e os recursos liberados após o uso
+                using (var conexao = new MySqlConnection(_conexaoMySQL))
+                {
+                    // Abre a conexão com o banco de dados MySQL
+                    conexao.Open();
+                    // Cria um novo comando SQL para atualizar dados na tabela 'cliente' com base no código
+                    MySqlCommand cmd = new MySqlCommand("Update cliente set NomeCli=@nome, TelCli=@telefone, EmailCli=@email " + " where CodCli=@codigo ", conexao);
+                    // Adiciona um parâmetro para o código do cliente a ser atualizado, definindo seu tipo e valor
+                    cmd.Parameters.Add("@codigo", MySqlDbType.Int32).Value = cliente.CodCli;
+                    // Adiciona um parâmetro para o novo nome, definindo seu tipo e valor
+                    cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = cliente.NomeCli;
+                    // Adiciona um parâmetro para o novo telefone, definindo seu tipo e valor
+                    cmd.Parameters.Add("@telefone", MySqlDbType.VarChar).Value = cliente.TelCli;
+                    // Adiciona um parâmetro para o novo email, definindo seu tipo e valor
+                    cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = cliente.EmailCli;
+                    // Executa o comando SQL de atualização e retorna o número de linhas afetadas
+                    //executa e verifica se a alteração foi realizada
+                    int linhasAfetadas = cmd.ExecuteNonQuery();
+                    return linhasAfetadas > 0; // Retorna true se ao menos uma linha foi atualizada
+
+                }
+            }
+            catch (MySqlException ex)
+            {
+                // Logar a exceção (usar um framework de logging como NLog ou Serilog)
+                Console.WriteLine($"Erro ao atualizar cliente: {ex.Message}");
+                return false; // Retorna false em caso de erro
+
+            }
+        }
 
     }
 }
